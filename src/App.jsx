f52b8b0554ad25a9be83f6e06e576ff1beb8d4a1
Bob Holland <30559674+SmagicJones@users.noreply.github.com>
2024-05-24 15:36:00 +0100

    styling header

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <Header title="Bob is cool" subtitle="There's just something about him" />
       <section>
-        <div className="grid md:grid-cols-2 gap-4">
+        <div className="grid md:grid-cols-2 gap-4 m-2">
           <div className="bg-slate-500 rounded-xl p-4">
             <h3>Testing</h3>
             <p>Some text</p>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 export default function Header({ title, subtitle }) {
   return (
     <>
-      <div className="text-center text-8xl p-4">
-        <h1>{title}</h1>
-        <h3>{subtitle}</h3>
+      <div className="grid md:grid-cols-1 gap-2 bg-lime-500 text-center">
+        <h1 className="text-8xl">{title}</h1>
+        <h3 className="text-4xl">{subtitle}</h3>
       </div>
     </>
   );
