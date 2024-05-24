18bc51df7f31d96727491824e69ac8d32d4ee0f5
Bob Holland <30559674+SmagicJones@users.noreply.github.com>
2024-05-24 15:06:14 +0100

    tailwind installed

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,17 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Bob Test</h1>
+      <h1 className="text-8xl">Bob Test</h1>
       <section>
-        <div>
-          <h3>Test push</h3>
+        <div className="grid grid-cols-2 gap-4">
+          <div className="bg-slate-500 rounded-xl p-4">
+            <h3>Testing</h3>
+            <p>Some text</p>
+          </div>
+          <div className="bg-slate-500 rounded-xl p-4">
+            <h3>Testing</h3>
+            <p>Some text</p>
+          </div>
         </div>
       </section>
     </>
